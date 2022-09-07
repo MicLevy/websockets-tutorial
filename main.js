c1818ef7a56efca0dd2beff557f268adb8759362
POLYGON\michael-L <michael@pt-eng.co.il>
2022-09-07 15:18:48 +0300

    fixes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { createBoard, playMove } from "./connect4.js";
 
 function getWebSocketServer() {
-  if (window.location.host === "MichaelPolygon.github.io") {
+  if (window.location.host === "michaelpolygon.github.io") {
     return "wss://connectfourblabla.herokuapp.com/";
   } else if (window.location.host === "localhost:8000") {
     return "ws://localhost:8001/";
